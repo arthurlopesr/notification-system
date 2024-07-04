09169531a254e1d1117b66cbed1a412cd6dbd98c
Arthur Lopes <arthurlopr12@gmail.com>
2024-07-04 20:50:49 -0300

    feat: create get method to notification

--- src/main/java/arthurlopes/notifications/presentation/controller/NotificationController.java
+++ src/main/java/arthurlopes/notifications/presentation/controller/NotificationController.java
@@ -1,18 +1,16 @@
 package arthurlopes.notifications.presentation.controller;
 
+import arthurlopes.notifications.domain.entity.NotificationEntity;
 import arthurlopes.notifications.presentation.dto.ScheduleNotificationDTO;
 import arthurlopes.notifications.service.NotificationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notification")
 public class NotificationController {
 
-    private NotificationService notificationService;
+    private final NotificationService notificationService;
 
     public NotificationController(NotificationService notificationService) {
         this.notificationService = notificationService;
@@ -22,5 +20,16 @@ public class NotificationController {
     public ResponseEntity<Void> scheduleNotification(@RequestBody ScheduleNotificationDTO scheduleNotificationDTO) {
         notificationService.scheduleNotification(scheduleNotificationDTO);
         return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/{notificationId}")
+    public ResponseEntity<NotificationEntity> getNotification(@PathVariable("notificationId") Long notificationId) {
+        var notification = notificationService.findById(notificationId);
+
+        if (notification.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(notification.get());
     }
 }
--- src/main/java/arthurlopes/notifications/service/NotificationService.java
+++ src/main/java/arthurlopes/notifications/service/NotificationService.java
@@ -1,8 +1,11 @@
 package arthurlopes.notifications.service;
 
+import arthurlopes.notifications.domain.entity.NotificationEntity;
 import arthurlopes.notifications.infra.repository.NotificationRepository;
 import arthurlopes.notifications.presentation.dto.ScheduleNotificationDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class NotificationService {
@@ -15,5 +18,9 @@ public class NotificationService {
 
     public void scheduleNotification(ScheduleNotificationDTO scheduleNotificationDTO) {
         notificationRepository.save(scheduleNotificationDTO.toNotificationEntity());
+    }
+
+    public Optional<NotificationEntity> findById(Long notificationId) {
+        return notificationRepository.findById(notificationId);
     }
 }
